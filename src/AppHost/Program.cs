800ee65ec2bed2605f551c0647ca97b92735ca11
Frank Tyler <frank.tyler@broadcom.com>
2025-04-30 16:01:00 -0400

    Add Keycloak container.

--- src/AppHost/Program.cs
+++ src/AppHost/Program.cs
@@ -8,9 +8,20 @@ var postgres = builder.AddPostgres("postgres")
 
 var productsDb = postgres.AddDatabase("productsdb");
 
+// Keycloak container
+var keycloak = builder.AddContainer("keycloak", "quay.io/keycloak/keycloak:24.0")
+    .WithEnvironment("KEYCLOAK_ADMIN", "admin")
+    .WithEnvironment("KEYCLOAK_ADMIN_PASSWORD", "admin")
+    .WithArgs("start-dev")
+    .WithHttpEndpoint(port: 8080, targetPort: 8080, name: "http")
+    .WithLifetime(ContainerLifetime.Persistent);
+
 var productApi = builder.AddProject<ProductCatalog>("product-api")
     .WithReference(productsDb)
-    .WaitFor(postgres);
+    .WaitFor(postgres)
+    .WithEnvironment("Keycloak__Authority", keycloak.GetEndpoint("http"))
+    .WithEnvironment("Keycloak__ClientId", "myapp-api")
+    .WithEnvironment("Keycloak__Secret", "myapp-secret");
 
 var blazorClient = builder.AddProject<Blazor>("blazor-client")
     .WithReference(productApi)
@@ -21,6 +32,7 @@ var gateway = builder.AddProject<Gateway>("gateway")
     .WaitFor(productApi)
     .WithReference(blazorClient)
     .WaitFor(blazorClient)
+    .WithEnvironment("Keycloak__Authority", keycloak.GetEndpoint("http"))
     .WithExternalHttpEndpoints();
 
 builder.Build().Run();
