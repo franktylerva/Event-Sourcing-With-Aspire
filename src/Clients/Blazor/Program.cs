b728d2beb1116b6a3309ef9ffc3ed5111e4762ec
Frank Tyler <frank.tyler@broadcom.com>
2025-05-01 10:05:52 -0400

    Add Keycloak configuration to Blazor frontend.

--- src/Clients/Blazor/Program.cs
+++ src/Clients/Blazor/Program.cs
@@ -1,11 +1,30 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Blazor;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddHttpClient("products_api_client",
+        client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
+    .AddHttpMessageHandler(sp =>
+    {
+        var handler = sp.GetRequiredService<AuthorizationMessageHandler>()
+            .ConfigureHandler(authorizedUrls: ["http://localhost:5062"]);
+            return handler;
+    });
+
+
+builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("products_api_client"));
+
+builder.Services.AddOidcAuthentication(options =>
+{
+    options.ProviderOptions.Authority = "http://localhost:8080/realms/event-sourcing";
+    options.ProviderOptions.ClientId = "blazor-client";
+    options.ProviderOptions.ResponseType = "code";
+    options.ProviderOptions.DefaultScopes.Add("blazor_api_scope");
+});
 
 await builder.Build().RunAsync();
